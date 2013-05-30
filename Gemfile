4e047ce6ad398c4da342807011cace2884edceed
Indykov Maksim <maksim@MacBook-Air-Indykov.local>
2013-05-30 21:26:03 +0400

    add active admin

--- Gemfile
+++ Gemfile
@@ -15,7 +15,6 @@ group :assets do
 
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
   gem 'therubyracer', :platforms => :ruby
-
   gem 'uglifier', '>= 1.0.3'
   gem 'compass-rails'
   gem 'zen-grids'
@@ -49,6 +48,7 @@ end
 gem 'jquery-rails'
 gem 'devise'
 gem 'simple_form'
+gem 'activeadmin'
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 BuildingInfo::Application.routes.draw do
+
+
+  root :to => 'pages#index'
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :build_objects
 
 
@@ -50,6 +55,7 @@ BuildingInfo::Application.routes.draw do
   get "pages/index"
 
   devise_for :users
+  ActiveAdmin.routes(self)
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -100,7 +106,7 @@ BuildingInfo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'pages#index'
+
 
   # See how all your routes lay out with "rake routes"
 
